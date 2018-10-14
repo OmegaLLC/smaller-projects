654eb3c5c52c01ea4d95d80707442bd23b5222f9
unknown <vitrovine@gmail.com>
2018-10-14 13:48:24 +0200

    update

--- program.cs
+++ program.cs
@@ -7,7 +7,7 @@ class ToDoList {
     private string filepath = "data.txt";
 
     public ToDoList() {
-        list = ReadFile();
+        list = ReadFile(); //empty list if no file or empty file, else it reads previous state from txt file
     }
     
     public void Add(string todo) {
@@ -67,29 +67,35 @@ class ToDoList {
 }
 
 public class ExecuteApp {
-    public static void Main(string[] args) { /* try to read state from previous run saved in a file */
+    public static void Main(string[] args) { // try to read state from previous run saved in a file 
         ToDoList todolist = new ToDoList();
         string input;
         while (true) {
             input = Console.ReadLine();
-            if(input == "Exit") {
+            if (input.Length < 4) {
+                Console.WriteLine("Command must be at least 4 char long");
+            }
+            else if(input.ToLower() == "exit") {
                 break;
             }
-            else if (input.Length <5) {
-                Console.WriteLine("input must be at least 5 characters"); /* minimal amount to get use any command (except exit), hinders indexing exceptions */
-            }
-            else if (input.Substring(0,3) == "Add") {
+            else if (input.Substring(0,3).ToLower() == "add" && input.Length >4) {
                 string added = input.Substring(4);
-                if (added[0] != '"' || added[added.Length-1] != '"' || added.Length<3) { /* if no quotation marks, dont accept. In the assignment it showed "" around what was added to the list, so I added this */
-                    Console.WriteLine("Put quotation marks around what you want to add");
+                if (added[0] != '"' || added[added.Length-1] != '"' || added.Length<3) { // if it doesnt have quote end and quote start. In the assignment it showed  
+                    //Console.WriteLine("Put quotation marks around what you want to add"); // "" around the strings added, but seeing as # was used weirdly too, I decided to allow both
+                    todolist.Add(added);                                                    // So if theres quote marks, then they are removed, if there arent then i just add what it says
                 } else {
                     added = added.Substring(0,added.Length-1); //remove quote end
                     added = added.Substring(1,added.Length-1); //remov quote start
                     todolist.Add(added); 
                 }
             }
-            else if (input.Substring(0,2) == "Do") {
-                string number = input.Substring(4);
+            else if (input.Substring(0,2).ToLower() == "do") { 
+                string number = input.Substring(3);
+                if (input[3] == '#') { // Assignment was confusing with how # is supposed to be used. task says that # is the number, but example shows usage of  Do #1, so i added both
+                    if (input.Length>4) {
+                        number = input.Substring(4);
+                    }
+                }
                 int number2;
                 if(int.TryParse(number, out number2)) {
                     todolist.Do(number2);
@@ -98,8 +104,13 @@ public class ExecuteApp {
                     Console.WriteLine("Not valid input for Do. Example: 'Do #2'");
                 }
             }
-            else if (input.Substring(0,5) == "Print") {
-                todolist.Print();
+            else if (input.Length >=5) {
+                if (input.Substring(0,5).ToLower() == "print") {
+                    todolist.Print();
+                }
+                else  {
+                Console.WriteLine(input+" is not a valid command");
+                }
             }
             else  {
                 Console.WriteLine(input+" is not a valid command");
